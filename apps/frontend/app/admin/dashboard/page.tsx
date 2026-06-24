bf44440c1a38a18edbe50eee8ce19b7adaa105de
OmRana2004 <omrana8755800322@gmail.com>
2026-06-25 00:57:34 +0530

    fav.ico

--- apps/frontend/app/admin/dashboard/page.tsx
+++ apps/frontend/app/admin/dashboard/page.tsx
@@ -4,7 +4,7 @@
 
 export default function Page() {
   return (
-      <h1 className="text-2xl font-bold text-slate-900 flex items-center justify-center gap-2">
+      <h1 className="bg-blue-300 text-6xl font-bold text-slate-900 flex items-center justify-center h-screen">
         Admin Dashboard
       </h1>
     
